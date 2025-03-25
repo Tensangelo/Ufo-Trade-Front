93720c9a7fd443deca6e785ddb48b200a701cc8b
Angelo Gaona <ang.dev@hotmail.com>
2025-03-25 06:40:59 -0500

    DEelete components layout and add layout code in pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,31 @@
-import AuthLayout from "@/components/layout/authLayout";
+"use client";
+
+import { useAuthContext  } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { AlertLoading } from "@/components/loading/verifySession";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return <AuthLayout>{children}</AuthLayout>;
+    const { user, loading, checkSession } = useAuthContext();
+    const router = useRouter();
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+        if (!user) {
+            checkSession(); // ✅ Llama `auth/me` solo si no hay usuario en memoria.
+        }
+    }, [user, checkSession]);
+
+    useEffect(() => {
+        setIsClient(true);
+
+        if (!loading && user) {
+            router.replace("/");  // Si ya está logueado, redirige a home
+        }
+    }, [user, loading, router]);
+
+    if (!isClient || loading) return <AlertLoading textInformation="Verificando sesión..." />
+
+    return <>{children}</>;
 }
